2799fe43c18df5f28bb35db04dcc05609173e9a3
mobisa-012 <mobisakwamboka@gmail.com>
2023-04-04 21:44:37 +0300

    scac revamp

---
+++ SCAC/scac_web_revamp/scac/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class ScacConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'scac'
---
+++ SCAC/scac_web_revamp/scac/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+# so far umerun?
+# eeeh, na imeonyesh logo, the background color and the links, though vertically
+# nimekosea wapi kwa hiyo css file?
+# wacha nione hio file kwa browser
+# wait babe do i need to run it again over here?
+# Create your models here.
+# okay.
+# but I thought it should run before I create the models?
+# let me first setup python here give me a minute
+# okay
+# hey babe, I think this site should have static files, so is there need to have a very complex db?
